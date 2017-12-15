f16672bb37204558011dac6ce22bdc24d35416e6
daamjad <daamjad@creativechaos.co>
2017-12-15 16:31:18 +0500

    Initial commit

---
+++ app/src/main/java/com/example/daamjad/bottomsheet/MainActivity.java
@@ -0,0 +1,78 @@
+package com.example.daamjad.bottomsheet;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomSheetBehavior sheetBehavior;
+    private Button btnBottomSheet;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setUpReferences();
+        setOnClickListener();
+
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        btnBottomSheet.setText(R.string.close);
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        btnBottomSheet.setText(R.string.expand);
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        sheetBehavior.setHideable(false);
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        sheetBehavior.setHideable(false);
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+    }
+
+    //region Helper method
+    private void setUpReferences() {
+        LinearLayout layoutBottomSheet = findViewById(R.id.bottom_sheet);
+        btnBottomSheet = findViewById(R.id.btn_bottom_sheet);
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+    }
+    //endregion
+
+    //region Helper methods for OnClick
+    private void setOnClickListener() {
+        btnBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                    btnBottomSheet.setText(R.string.close);
+                } else {
+                    sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                    btnBottomSheet.setText(R.string.expand);
+                }
+            }
+        });
+    }
+    //endregion
+}
